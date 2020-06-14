c5a497a5cbd424231efc43f5e55fa7ea7a36c115
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-14 19:24:46 +0300

    add blueprint class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+  showDescription: boolean;
+  constructor(public quote: string, public author: string, public submitter: string, public upvote: number, public downvote: number, public date: Date){
+    this.showDescription=false;
+  }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes: Quotes[] = [
+    new Quotes('Do I not destroy my enemies when I make them my friends?', 'Abraham Lincoln', 'Mathews Alufwani', 0, 0, new Date(2020,11,11)),
+  ];
 
   constructor() { }
 
